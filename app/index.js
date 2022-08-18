6ba0bd8b01b734b104a93035be60ad302b9ffddc
eminerce57 <83703865+eminerce57@users.noreply.github.com>
2022-08-18 13:21:38 +0300

    added array function

--- app/index.js
+++ app/index.js
@@ -11,3 +11,20 @@ const { log: myLog } = console;
 myLog("hello world!","(in mylog)")
 
 myLog("hi","(in mylog)")
+
+
+const { table: tableView } = console;
+const arr = [1, 2, 3, 4, 5];
+
+tableView(arr);
+
+
+
+
+
+// console group
+console.group("groupName");
+console.log("hi");
+console.log("testing");
+console.groupEnd();
+// console group end
